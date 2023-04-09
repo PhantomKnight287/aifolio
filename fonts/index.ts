47722de4e835b37d8e7416e59768bc7739701bc8
Gurpal Singh <gillgurpal699@gmail.com>
2023-04-09 13:53:04 +0530

    create `fonts/index.ts` to export fonts

---
+++ fonts/index.ts
@@ -0,0 +1,7 @@
+import { Sen } from "next/font/google";
+export const sen = Sen({
+  weight: ["400", "700", "800"],
+  display: "swap",
+  subsets: ["latin", "latin-ext"],
+  variable: "--font-sen",
+});
