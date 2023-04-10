3e970edba5a9634d6a1bf6fb5f0f01314daae1fb
Gurpal Singh <gillgurpal699@gmail.com>
2023-04-10 12:30:47 +0530

    [frontend] update types of supabase

--- frontend/types/supabase.ts
+++ frontend/types/supabase.ts
@@ -4,46 +4,49 @@ export type Json =
   | boolean
   | null
   | { [key: string]: Json }
-  | Json[]
+  | Json[];
 
 export interface Database {
   public: {
     Tables: {
       portfolios: {
         Row: {
-          avatarUrl: string | null
-          created_at: string | null
-          id: number
-          portfolio: string | null
-          username: string
-        }
+          avatarUrl: string | null;
+          created_at: string | null;
+          id: number;
+          portfolio: string | null;
+          tries: number;
+          username: string;
+        };
         Insert: {
-          avatarUrl?: string | null
-          created_at?: string | null
-          id?: number
-          portfolio?: string | null
-          username: string
-        }
+          avatarUrl?: string | null;
+          created_at?: string | null;
+          id?: number;
+          portfolio?: string | null;
+          tries?: number;
+          username: string;
+        };
         Update: {
-          avatarUrl?: string | null
-          created_at?: string | null
-          id?: number
-          portfolio?: string | null
-          username?: string
-        }
-      }
-    }
+          avatarUrl?: string | null;
+          created_at?: string | null;
+          id?: number;
+          portfolio?: string | null;
+          tries?: number;
+          username?: string;
+        };
+      };
+    };
     Views: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     Functions: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     Enums: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     CompositeTypes: {
-      [_ in never]: never
-    }
-  }
+      [_ in never]: never;
+    };
+  };
 }
